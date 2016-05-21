0a2eeb5152a22a7cff854fc7859f55e510b9ebdb
Johnny Loi <johnnyloi818@gmail.com>
2016-05-21 00:56:38 -0700

    root now redirects to casetabs show path

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :tweets, param: :name, only: :show
   resources :twitter_users, param: :name, only: :show
 
-  root to: 'tweets#show', name: 'casetabs'
+  get '/', to: redirect('/tweets/casetabs')
 end
